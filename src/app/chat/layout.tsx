ac90cfd65692c7e9ab659416d9a1e31d7393e7d0
Nabeel Ahmed <nabeelahmed@dechains.com>
2024-03-19 16:35:37 +0500

    fix: pdf worker issue fixed

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import DocGroup, { IDocGroup } from "@/schemas/DocGroup";
 import Sidebar from "@/components/Sidebar/Sidebar";
 import { authOptions } from "@/utils/authOptions";
+import PDFWorker from "@/components/PDFWorker/PDFWorker";
 
 export const revalidate = 3600;
 
@@ -13,6 +14,7 @@ const ChatLayout = async ({ children }: { children: React.ReactNode }) => {
 
   return (
     <div style={{ display: "flex", height: "100vh" }}>
+      <PDFWorker />
       <Sidebar groups={data} />
 
       {children}
