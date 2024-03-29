7ea77272ca5dd3969eac15b57b9e7c3b1fcb4fde
Nabeel Ahmed <nabeelahmed@dechains.com>
2024-03-29 16:42:10 +0500

    chore: group thumbnail counter show if files are more than 4

--- src/components/GroupThumbnail/GroupThumbnail.tsx
+++ src/components/GroupThumbnail/GroupThumbnail.tsx
@@ -3,8 +3,15 @@ import Link from "next/link";
 import React from "react";
 import { Document as PDFDoc, Page } from "react-pdf";
 import styles from "./GroupThumbnail.module.css";
+import { Badge } from "@mui/material";
 
 const GroupThumbnail = ({ group }: { group: IDocGroupWithFileUrls }) => {
+  const fileUrls = group.fileUrls.filter((_, i) => i <= 3);
+
+  const remainingFiles = group.fileUrls.length - fileUrls.length;
+
+  console.log("remainingFiles", remainingFiles);
+
   return (
     <Link
       key={group.groupId}
@@ -18,21 +25,23 @@ const GroupThumbnail = ({ group }: { group: IDocGroupWithFileUrls }) => {
         alignItems: "center",
       }}
     >
-      <div className={styles.groupContainer}>
-        {group.fileUrls.map((url, i) => {
-          return (
-            <PDFDoc file={url} className={styles.pdfDoc} key={i} renderMode="canvas" loading="Generating Thumbnail">
-              <Page
-                key={`page_1`}
-                pageNumber={1}
-                className={styles.pdfPage}
-                renderTextLayer={false}
-                renderAnnotationLayer={false}
-              />
-            </PDFDoc>
-          );
-        })}
-      </div>
+      <Badge invisible={remainingFiles === 0} badgeContent={`+${remainingFiles}`} color="primary">
+        <div className={fileUrls.length === 1 ? styles.groupContainerFixed : styles.groupContainer}>
+          {fileUrls.map((url, i) => {
+            return (
+              <PDFDoc file={url} className={styles.pdfDoc} key={i} renderMode="canvas" loading="Generating Thumbnail">
+                <Page
+                  key={`page_1`}
+                  pageNumber={1}
+                  className={styles.pdfPage}
+                  renderTextLayer={false}
+                  renderAnnotationLayer={false}
+                />
+              </PDFDoc>
+            );
+          })}
+        </div>
+      </Badge>
       <h3 style={{ textAlign: "center" }}>{group.groupName}</h3>
     </Link>
   );
