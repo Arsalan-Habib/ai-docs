8d675da892b34209358df02f174573f87df86b07
Nabeel Ahmed <nabeelahmed@dechains.com>
2024-04-04 13:00:45 +0500

    chore: upload-document href removed

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -26,7 +26,7 @@ const Sidebar = ({ groups }: SidebarProps) => {
       </Link>
       <SidebarTabs />
 
-      <Link href="/upload-documents">
+      <Link href="/library">
         <button className={styles.newchatbtn}>
           <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
             <path
